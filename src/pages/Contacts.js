27ec1063d74370fe91e194d7aaca6e15071fbb0d
bukharov <dilmurod.bukharov@gmail.com>
2021-10-08 12:41:57 -0400

    added fontawesome icons

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,15 +1,46 @@
 import { useEffect } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(fab, fas, far);
+
 const Contacts = () => {
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
   return (
     <div>
-      <h1>Contacts</h1>
-      <p>
-        E-Mail dilmurod.bukharov@gmail.com Phone Mobile Number: (412)758-1167
-        GitHub LinkedIn
-      </p>
+      <h1>Contact</h1>
+      <h3>E-Mail</h3>
+      <p>dilmurod.bukharov@gmail.com</p>
+      <h3>Phone</h3>
+      <p>Mobile Number: (412)758-1167</p>
+      <div style={{ display: `flex`, flexDirection: `row` }}>
+        <div style={{ marginRight: `30px` }}>
+          <h4>GitHub</h4>
+          <a
+            href="https://github.com/dilmurodb"
+            target="_blank"
+            rel="noopener"
+            style={{ color: `black` }}
+          >
+            <FontAwesomeIcon icon={["fab", "github-square"]} size="3x" />
+          </a>
+        </div>
+        <div>
+          <h4>LinkedIn</h4>
+          <a
+            href="https://www.linkedin.com/in/d-bukharov/"
+            target="_blank"
+            rel="noopener"
+            style={{ color: `black` }}
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,59 @@
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
+import { faTasks } from "@fortawesome/free-solid-svg-icons";
+import { faTh } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+
 const Portfolio = () => {
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
   return (
     <div>
-      <h1>Portfolio</h1>
-      <p>
-        These are some of the projects I have built so far. Please click the
-        Icons below to see my projects.
-      </p>
+      <div style={{ margin: `50px`, backgroundColor: "green" }}>
+        <a
+          href="https://dilmurodb.github.io/trivia/"
+          target="_blank"
+          rel="noopener"
+          style={{ color: `white` }}
+        >
+          <FontAwesomeIcon icon={faBookOpen} size="5x" />
+        </a>
+      </div>
+      <p>---</p>
+      <div style={{ margin: `50px`, backgroundColor: "green" }}>
+        <a
+          href="https://bukharov-todo-list.herokuapp.com/"
+          target="_blank"
+          rel="noopener"
+          style={{ color: `white` }}
+        >
+          <FontAwesomeIcon icon={faTasks} size="5x" />
+        </a>
+      </div>
+      <p>---</p>
+      <div style={{ margin: `50px`, backgroundColor: "green" }}>
+        <a
+          href="https://dilmurodb.github.io/tic-tac/"
+          target="_blank"
+          rel="noopener"
+          style={{ color: `white` }}
+        >
+          <FontAwesomeIcon icon={faTh} size="5x" />
+        </a>
+      </div>
+      <p>---</p>
+      <div style={{ margin: `50px`, backgroundColor: "green" }}>
+        <a
+          href="https://musing-fermi-856938.netlify.com/"
+          target="_blank"
+          rel="noopener"
+          style={{ color: `white` }}
+        >
+          <FontAwesomeIcon icon={faLightbulb} size="5x" />
+        </a>
+      </div>
     </div>
   );
 };
